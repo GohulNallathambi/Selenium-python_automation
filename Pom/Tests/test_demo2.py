54787b9a3d168f181029707cd7d710af998478f7
GohulNallathambi <103426884+GohulNallathambi@users.noreply.github.com>
2023-04-18 14:59:20 +0530

    Update test_demo2.py

--- Pom/Tests/test_demo2.py
+++ Pom/Tests/test_demo2.py
@@ -1,12 +1,12 @@
-import pytest
-from Pom.Pages.login import LoginPage
+# import pytest
+# from Pom.Pages.login import LoginPage
 
 
-@pytest.mark.usefixtures('setup')
-class TestWaitsDemo2:
-    def test_page(self):
-        login = LoginPage(self.driver)
-        login.test_do_login()
-        login.scroll_down()
+# @pytest.mark.usefixtures('setup')
+# class TestWaitsDemo2:
+#     def test_page(self):
+#         login = LoginPage(self.driver)
+#         login.test_do_login()
+#         login.scroll_down()
 
 
