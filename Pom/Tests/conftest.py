76c8002388828f2241c6b1fcc1faac1858132f02
GohulNallathambi <103426884+GohulNallathambi@users.noreply.github.com>
2023-04-19 12:52:03 +0530

    Update conftest.py

--- Pom/Tests/conftest.py
+++ Pom/Tests/conftest.py
@@ -1,12 +1,10 @@
 import pytest
 from selenium import webdriver
-from Pom.Config.config import TestData
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.edge.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
-
 
 @pytest.fixture(scope="class")
 # def setup(request):
@@ -19,16 +17,16 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 #     driver.close()
 
 def setup(request, browser, url):
-    if browser == "chromium":
-        options = Options()
-        options.add_argument("--ignore-ssl-errors=yes")
+    if browser == "chrome":
+        options = webdriver.ChromeOptions()
         options.add_argument("--ignore-certificate-errors")
-        capabilities = options.capabilities
-        driver = webdriver.Chrome(executable_path=TestData.Executable_Path, options=options)
+        capabilities = options.to_capabilities()
+        capabilities['acceptInsecureCerts'] = True
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install(), desired_capabilities=capabilities)
     elif browser == "firefox":
         driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     elif browser == "edge":
-        options = Options()
+        options = Options
         options.accept_insecure_certs = True
         driver = webdriver.Edge(executable_path=EdgeChromiumDriverManager().install(), options=options)
     driver.get(url)
@@ -40,7 +38,7 @@ def setup(request, browser, url):
 
 def pytest_addoption(parser):
     parser.addoption("--web-browser", default="chrome", help="Browser to run the tests in (chrome, firefox, edge)")
-    parser.addoption("--url", default="http://localhost:8000", help="Base URL for the application being tested")
+    parser.addoption("--url", default="http://localhost:8080", help="Base URL for the application being tested")
 
 
 @pytest.fixture(scope="class", autouse=True)
