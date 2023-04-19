89e6d88417b496f36855cfb8866ee98d43213bd7
GohulNallathambi <103426884+GohulNallathambi@users.noreply.github.com>
2023-04-19 12:52:43 +0530

    Delete Pom directory

--- Pom/Config/config.py
+++
@@ -1,6 +0,0 @@
-
-class TestData:
-    Executable_Path = "C:\\Users\\Gohul\\PycharmProjects\\driver\\chromedriver.exe"
-    BASE_URL = "https://43.205.255.9/"
-    USER_NAME = "admin"
-    PASSWORD = "Admin@1234"
