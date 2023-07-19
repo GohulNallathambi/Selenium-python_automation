7c9ba44b6e6fb23f82b252b11a6e5a6a607f8986
GohulNallathambi <103426884+GohulNallathambi@users.noreply.github.com>
2023-07-19 18:12:44 +0530

    Update host.py

--- Pom/Pages/host.py
+++ Pom/Pages/host.py
@@ -23,6 +23,22 @@ class HostPage(BaseDriver):
         search.send_keys(Keys.ENTER)
         time.sleep(2)
 
+    def host_alert(self):
+        self.driver.find_element(By.XPATH, "//div[@class='row']/div[3]/div/a/div/div/div").click()
+        time.sleep(5)
+        self.driver.find_element(By.XPATH,"//*[contains(text(),'Results per page:')]/select/option[4]").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//thead/tr[1]/th[1]/input[1]").click()
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, "//th[contains(text(),'Resolve')]").click()
+        time.sleep(4)
+        self.driver.find_element(By.XPATH, " //body/app-root[1]/app-global[1]/div[1]/app-alerts[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]/label[2]/span[1]").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//textarea").send_keys("Legitimate")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//*[contains(text(),'Submit')]").click()
+        time.sleep(3)
+
 
         # time.sleep(2)
         # self.driver.find_element(By.XPATH, "//*[@id='Management']/a/span[2]").click()
