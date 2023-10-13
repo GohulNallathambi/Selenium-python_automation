866289ccef59b50602dc5b32de630aae6e7ecccb
GohulNallathambi <103426884+GohulNallathambi@users.noreply.github.com>
2023-10-13 19:52:30 +0530

    Update config.py

--- Pom/Config/config.py
+++ Pom/Config/config.py
@@ -2,5 +2,5 @@
 class TestData:
     Executable_Path = "C:\\Users\\Gohul\\PycharmProjects\\driver\\chromedriver.exe"
     BASE_URL = "https://43.205.255.9/"
-    USER_NAME = "analyst"
-    PASSWORD = "Analyst@789"
+    USER_NAME = "analyst321"
+    PASSWORD = "Analyst@990"
